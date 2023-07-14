9f47301ef76b6bb7679244f62a7ca6e73e14024b
Deibson Cogo <deibsoncogo@outlook.com>
2023-07-14 17:37:59 -0300

    ✅ test: criação dos testes e2e

---
+++ tests/app.e2e-spec.ts
@@ -0,0 +1,25 @@
+import { INestApplication } from '@nestjs/common'
+import { Test, TestingModule } from '@nestjs/testing'
+import * as request from 'supertest'
+import { AppModule } from '../src/app/app.module'
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleFixture.createNestApplication()
+
+    await app.init()
+  })
+
+  it('/ (GET)', async () => {
+    const response = await request(app.getHttpServer()).get('/')
+
+    expect(response.status).toEqual(200)
+    expect(response.body.status).toEqual('Rota executada com sucesso')
+  })
+})
