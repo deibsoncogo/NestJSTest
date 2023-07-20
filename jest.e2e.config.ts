3bcca0c8f1f22dde7dc151275922963803df0e8e
Deibson Cogo <deibsoncogo@outlook.com>
2023-07-20 19:41:21 -0300

    ✅ test: alteração nas configurações dos testes

--- jest.e2e.config.ts
+++ jest.e2e.config.ts
@@ -3,9 +3,9 @@ import type { Config } from 'jest'
 const config: Config = {
   moduleFileExtensions: ['ts', 'js', 'json'],
   rootDir: './src/modules/',
-  roots: ['../../tests/unitary/'],
-  testRegex: ['.*\\.spec\\.ts$'],
-  coverageDirectory: '../../tests/unitary/coverage',
+  roots: ['../../tests/e2e/'],
+  testRegex: ['.*\\.e2e\\.spec\\.ts$'],
+  coverageDirectory: '../../tests/e2e/coverage',
   collectCoverageFrom: ['**/*.(t|j)s'],
   transform: { '^.+\\.(t|j)s$': 'ts-jest' },
   testEnvironment: 'node',
